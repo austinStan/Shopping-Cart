5f2a3f8f1f28e93977265f02ba39227451272b79
AustinStan <austinstan24@gmail.com>
2021-10-03 01:06:25 +0300

    Init

---
+++ App.js
@@ -0,0 +1,31 @@
+import React ,{useState} from 'react';
+import {View, Text,StyleSheet, FlatList} from 'react-native';
+import  Header  from './components/Header';
+import ListItem from './components/ListItem';
+
+const App = () => {
+  const [items, setItems] = useState([
+    {id: 1, text: 'Milk'},
+    {id: 2, text: 'Eggs'},
+    {id: 3, text: 'Bread'},
+    {id: 4, text: 'Cheese'},
+  ]);
+
+  return (
+    <View style={styles.container}>
+      <Header title="Shopping List"/>
+      <FlatList
+        data={items}
+        renderItem={({item}) => <ListItem item={item}/>}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop:60,
+  },
+});
+export default App;
